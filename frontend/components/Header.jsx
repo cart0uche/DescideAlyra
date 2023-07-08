1cc084dd9893988664e89095da30b0f2de0b7157
Hafid <hafid.saou@gmail.com>
2023-07-08 19:51:59 +0200

    add logo and litepaper

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -2,30 +2,51 @@ import React from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Flex, Text, Spacer } from "@chakra-ui/react";
 import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => {
    return (
-      <Flex p="2rem" justifyContent="flex-start" alignItems="center">
-         <Link href="/">
-            <Text
-               fontSize="1.5rem"
-               fontWeight="bold"
-               color="blue.500"
-               cursor="pointer"
-            >
-               Home
-            </Text>
-         </Link>
-         <Text
-            fontSize="1.5rem"
-            fontWeight="bold"
-            color="blue.500"
-            cursor="pointer"
-            ml={10}
-         >
-            Litepaper
-         </Text>
-         <Link href="/researcher">
+      <Flex
+         p="2rem"
+         justifyContent="space-between"
+         alignItems="center"
+         width="100%"
+         h="15vh"
+      >
+         <Image src="/alyra.png" width={100} height={100} alt="alyra" />
+         <Flex gap={3} ml={10}>
+            <Link href="/">
+               <Text
+                  fontSize="1.5rem"
+                  fontWeight="bold"
+                  color="blue.500"
+                  cursor="pointer"
+               >
+                  DeScide
+               </Text>
+            </Link>
+            <Link href="/DescideLitepaper.pdf">
+               <Text
+                  fontSize="1.5rem"
+                  fontWeight="bold"
+                  color="blue.500"
+                  cursor="pointer"
+                  ml={10}
+               >
+                  Litepaper
+               </Text>
+            </Link>
+            <Link href="/researcher">
+               <Text
+                  fontSize="1.5rem"
+                  fontWeight="bold"
+                  color="blue.500"
+                  cursor="pointer"
+                  ml={10}
+               >
+                  Researcher
+               </Text>
+            </Link>
             <Text
                fontSize="1.5rem"
                fontWeight="bold"
@@ -33,29 +54,20 @@ const Header = () => {
                cursor="pointer"
                ml={10}
             >
-               Researcher
+               Project
             </Text>
-         </Link>
-         <Text
-            fontSize="1.5rem"
-            fontWeight="bold"
-            color="blue.500"
-            cursor="pointer"
-            ml={10}
-         >
-            Project
-         </Text>
-         <Link href="/admin">
-            <Text
-               fontSize="1.5rem"
-               fontWeight="bold"
-               color="blue.500"
-               cursor="pointer"
-               ml={10}
-            >
-               Admin
-            </Text>
-         </Link>
+            <Link href="/admin">
+               <Text
+                  fontSize="1.5rem"
+                  fontWeight="bold"
+                  color="blue.500"
+                  cursor="pointer"
+                  ml={10}
+               >
+                  Admin
+               </Text>
+            </Link>
+         </Flex>
 
          <Flex marginLeft="auto">
             <ConnectButton />
