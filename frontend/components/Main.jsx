bdea3547c7befebac4c0b66b72b41a5246b1d6f0
Hafid <hafid.saou@gmail.com>
2023-07-16 00:14:42 +0200

    fix error rendering issue

--- frontend/components/Main.jsx
+++ frontend/components/Main.jsx
@@ -2,25 +2,19 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import SelectAction from "./SelectAction";
-import { useAccount } from "wagmi";
 
 function Main() {
-   const { address: addrAccount, isConnected } = useAccount();
    const [mounted, setMounted] = useState(false);
 
    useEffect(() => {
       setMounted(true);
    }, []);
 
-   if (!isConnected) {
-      return <div></div>;
-   }
-
    if (!mounted) return <></>;
 
    return (
       <div>
-         <SelectAction />
+         <SelectAction/>
       </div>
    );
 }
