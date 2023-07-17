033a8f91d37a8fc245deba5b72332cdf3375e2de
Hafid <hafid.saou@gmail.com>
2023-07-17 19:36:59 +0200

    add button to vote

--- frontend/components/OneFundsRequest.jsx
+++ frontend/components/OneFundsRequest.jsx
@@ -3,10 +3,25 @@ import { v4 as uuidv4 } from "uuid";
 import { useFundRequest } from "@/hooks/useFundRequest";
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
+import {
+   ButtonGroup,
+   useToast,
+   Popover,
+   PopoverTrigger,
+   PopoverContent,
+   PopoverHeader,
+   PopoverBody,
+   PopoverFooter,
+   PopoverArrow,
+   PopoverCloseButton,
+   useDisclosure,
+} from "@chakra-ui/react";
+import { MdOutlineHowToVote } from "react-icons/md";
 
 function OneFundsRequest({ fundsRequest }) {
+   const { isOpen, onToggle, onClose } = useDisclosure();
    const { getFundsRequestDetails, fundsRequestDetail } = useFundRequest();
-   console.log("fundsRequest", fundsRequest);
+   const { addVote, isLoadingAddVote } = useFundRequest();
 
    useEffect(() => {
       getFundsRequestDetails(fundsRequest.fundsRequestId);
@@ -46,7 +61,63 @@ function OneFundsRequest({ fundsRequest }) {
             ETH
          </Td>
          <Td>
-            <Button>Vote</Button>
+            <Popover
+               returnFocusOnClose={false}
+               isOpen={isOpen}
+               onClose={onClose}
+               placement="right"
+               closeOnBlur={false}
+            >
+               <PopoverTrigger>
+                  <Button
+                     leftIcon={<MdOutlineHowToVote size={20} />}
+                     type="submit"
+                     variant="solid"
+                     colorScheme="blue"
+                     onClick={onToggle}
+                     isLoading={isLoadingAddVote}
+                  >
+                     Vote
+                  </Button>
+               </PopoverTrigger>
+
+               <PopoverContent>
+                  <PopoverHeader fontWeight="semibold">
+                     Confirmation
+                  </PopoverHeader>
+                  <PopoverArrow />
+                  <PopoverCloseButton />
+                  <PopoverBody>
+                     Do you vote in favor of this request?
+                  </PopoverBody>
+                  <PopoverFooter display="flex" justifyContent="flex-end">
+                     <ButtonGroup size="sm">
+                        <Button
+                           variant="outline"
+                           onClick={() => {
+                              addVote({
+                                 args: [fundsRequest.fundsRequestId, false],
+                              });
+                              onClose();
+                           }}
+                        >
+                           No
+                        </Button>
+                        <Button
+                           colorScheme="red"
+                           onClick={() => {
+                              addVote({
+                                 args: [fundsRequest.fundsRequestId, true],
+                              });
+                              onClose();
+                           }}
+                        >
+                           Yes
+                        </Button>
+                     </ButtonGroup>
+                  </PopoverFooter>
+               </PopoverContent>
+            </Popover>
          </Td>
       </Tr>
    );
--- frontend/hooks/useFundRequest.js
+++ frontend/hooks/useFundRequest.js
@@ -36,45 +36,69 @@ export function useFundRequest() {
       });
 
    const { write: createFundsRequest, isLoadingCreateFundsRequest } =
-         useContractWrite({
-            address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
-            abi: Contract.abi,
-            functionName: "createFundsRequest",
-            onError: (error) => {
-               console.log(error);
-               toast({
-                  status: "error",
-                  isClosable: true,
-                  position: "top-middle",
-                  title: "createFundsRequest failed",
-                  description: error.message,
-               });
-            },
-            onSuccess: (data) => {
-               toast({
-                  status: "info",
-                  isClosable: true,
-                  position: "top-middle",
-                  title: "createFundsRequest success",
-               });
-            },
-         });      
+      useContractWrite({
+         address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+         abi: Contract.abi,
+         functionName: "createFundsRequest",
+         onError: (error) => {
+            console.log(error);
+            toast({
+               status: "error",
+               isClosable: true,
+               position: "top-middle",
+               title: "createFundsRequest failed",
+               description: error.message,
+            });
+         },
+         onSuccess: (data) => {
+            toast({
+               status: "info",
+               isClosable: true,
+               position: "top-middle",
+               title: "createFundsRequest success",
+            });
+         },
+      });
 
-      const getFundsRequestDetails = async (fundRequestId) => {
-         const data = await readContract({
-            address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
-            abi: Contract.abi,
-            functionName: "getFundsRequestDetails",
-            onError(error) {
-               console.log("Error", error);
-            },
-            onSuccess(data) {},
-            args: [Number(fundRequestId)],
-            account: addressAccount,
+   const getFundsRequestDetails = async (fundRequestId) => {
+      const data = await readContract({
+         address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+         abi: Contract.abi,
+         functionName: "getFundsRequestDetails",
+         onError(error) {
+            console.log("Error", error);
+         },
+         onSuccess(data) {},
+         args: [Number(fundRequestId)],
+         account: addressAccount,
+      });
+      setFundsRequestDetail(data);
+   };
+
+   // créer une fonction pour voter
+   const { write: addVote, isLoadingAddVote } = useContractWrite({
+      address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+      abi: Contract.abi,
+      functionName: "addVote",
+      onError: (error) => {
+         console.log(error);
+         toast({
+            status: "error",
+            isClosable: true,
+            position: "top-middle",
+            title: "addVote failed",
+            description: error.message,
          });
-         setFundsRequestDetail(data);
-      };
-   
+      },
+      onSuccess: (data) => {
+         toast({
+            status: "info",
+            isClosable: true,
+            position: "top-middle",
+            title: "addVote success",
+         });
+      },
+   });
 
    return {
       openFundsRequest,
@@ -83,5 +107,7 @@ export function useFundRequest() {
       isLoadingCreateFundsRequest,
       getFundsRequestDetails,
       fundsRequestDetail,
+      addVote,
+      isLoadingAddVote,
    };
 }
