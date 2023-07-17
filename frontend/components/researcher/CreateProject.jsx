4033b9811d8655e839341e9b53075e90a433470c
hafid <h.saou@archeon-medical.com>
2023-07-17 13:57:35 +0200

    project creation : active button only when the 2 files are uploaded

--- frontend/components/researcher/CreateProject.jsx
+++ frontend/components/researcher/CreateProject.jsx
@@ -1,123 +1,139 @@
 import { useEffect, useState } from "react";
 import {
-   FormControl,
-   FormLabel,
-   Input,
-   Button,
-   VStack,
-   Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  VStack,
+  Box,
 } from "@chakra-ui/react";
 import { useProject } from "@/hooks/useProject";
 import UploadIPFS from "@/components/utils/UploadIPFS";
 
 function CreateProject() {
-   const { createProject, isLoadingCreateProject } = useProject();
-   const [inputValue, setInputValue] = useState({
-      projectTitle: "",
-      projectDescription: "",
-      amountAsked: "",
-      imageUrl: "",
-      projectDetailsUri: "",
-   });
+  const { createProject, isLoadingCreateProject } = useProject();
+  const [activeButton, setActiveButton] = useState(false);
+  const [isImageUploaded, setIsImageUploaded] = useState(false);
+  const [isLitepaperUploaded, setIsLitepaperUploaded] = useState(false);
+  const [inputValue, setInputValue] = useState({
+    projectTitle: "",
+    projectDescription: "",
+    amountAsked: "",
+    imageUrl: "",
+    projectDetailsUri: "",
+  });
 
-   const handleChange = (e) => {
-      const { name, value } = e.target;
-      setInputValue((prevData) => ({
-         ...prevData,
-         [name]: value,
-      }));
-   };
+  useEffect(() => {
+    if (isImageUploaded && isLitepaperUploaded) {
+      setActiveButton(true);
+    }
+  }, [isImageUploaded, isLitepaperUploaded]);
 
-   const setFileUrlImage = (url) => {
-      setInputValue((prevData) => ({
-         ...prevData,
-         imageUrl: url,
-      }));
-   };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-   const setFileUrlLitepaper = ( url) => {
-      setInputValue((prevData) => ({
-         ...prevData,
-         projectDetailsUri: url,
-      }));
-   };
+  const setFileUrlImage = (url) => {
+    setInputValue((prevData) => ({
+      ...prevData,
+      imageUrl: url,
+    }));
+  };
 
-   const handleSubmit = (e) => {
-      e.preventDefault();
-      createProject({
-         args: [
-            inputValue.projectTitle,
-            inputValue.projectDescription,
-            inputValue.imageUrl,
-            inputValue.amountAsked,
-            inputValue.projectDetailsUri,
-         ],
-      });
-   };
+  const setFileUrlLitepaper = (url) => {
+    setInputValue((prevData) => ({
+      ...prevData,
+      projectDetailsUri: url,
+    }));
+  };
 
-   return (
-      <div>
-         <Box
-            p={4}
-            borderWidth="1px"
-            borderRadius="md"
-            maxW="400px"
-            mx="auto"
-            boxShadow="md"
-            mt={20}
-         >
-            <VStack spacing={4}>
-               <FormControl id="projectTitle">
-                  <FormLabel>Project name</FormLabel>
-                  <Input
-                     name="projectTitle"
-                     type="text"
-                     value={inputValue.projectTitle}
-                     onChange={handleChange}
-                  />
-               </FormControl>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createProject({
+      args: [
+        inputValue.projectTitle,
+        inputValue.projectDescription,
+        inputValue.imageUrl,
+        inputValue.amountAsked,
+        inputValue.projectDetailsUri,
+      ],
+    });
+  };
 
-               <FormControl id="projectDescription">
-                  <FormLabel>Project description</FormLabel>
-                  <Input
-                     name="projectDescription"
-                     type="text"
-                     value={inputValue.projectDescription}
-                     onChange={handleChange}
-                  />
-               </FormControl>
+  return (
+    <div>
+      <Box
+        p={4}
+        borderWidth="1px"
+        borderRadius="md"
+        maxW="400px"
+        mx="auto"
+        boxShadow="md"
+        mt={20}
+      >
+        <VStack spacing={4}>
+          <FormControl id="projectTitle">
+            <FormLabel>Project name</FormLabel>
+            <Input
+              name="projectTitle"
+              type="text"
+              value={inputValue.projectTitle}
+              onChange={handleChange}
+            />
+          </FormControl>
 
-               <FormControl id="amountAsked">
-                  <FormLabel>Amount asked</FormLabel>
-                  <Input
-                     name="amountAsked"
-                     type="text"
-                     value={inputValue.amountAsked}
-                     onChange={handleChange}
-                  />
-               </FormControl>
+          <FormControl id="projectDescription">
+            <FormLabel>Project description</FormLabel>
+            <Input
+              name="projectDescription"
+              type="text"
+              value={inputValue.projectDescription}
+              onChange={handleChange}
+            />
+          </FormControl>
 
-               <FormControl id="image">
-                  <FormLabel>Image</FormLabel>
-                  <UploadIPFS setFileUrl={setFileUrlImage} />
-               </FormControl>
+          <FormControl id="amountAsked">
+            <FormLabel>Amount asked</FormLabel>
+            <Input
+              name="amountAsked"
+              type="text"
+              value={inputValue.amountAsked}
+              onChange={handleChange}
+            />
+          </FormControl>
 
-               <FormControl id="image">
-                  <FormLabel>Litepaper</FormLabel>
-                  <UploadIPFS setFileUrl={setFileUrlLitepaper} />
-               </FormControl>
+          <FormControl id="image">
+            <FormLabel>Image</FormLabel>
+            <UploadIPFS
+              setFileUrl={setFileUrlImage}
+              setUploaded={setIsImageUploaded}
+            />
+          </FormControl>
 
-               <Button
-                  colorScheme="blue"
-                  onClick={handleSubmit}
-                  isLoading={isLoadingCreateProject}
-               >
-                  Valider
-               </Button>
-            </VStack>
-         </Box>
-      </div>
-   );
+          <FormControl id="image">
+            <FormLabel>Litepaper</FormLabel>
+            <UploadIPFS
+              setFileUrl={setFileUrlLitepaper}
+              setUploaded={setIsLitepaperUploaded}
+            />
+          </FormControl>
+
+          <Button
+            colorScheme="blue"
+            onClick={handleSubmit}
+            isLoading={isLoadingCreateProject}
+            isDisabled={!activeButton}
+          >
+            Valider
+          </Button>
+        </VStack>
+      </Box>
+    </div>
+  );
 }
 
 export default CreateProject;
--- frontend/components/utils/UploadIPFS.jsx
+++ frontend/components/utils/UploadIPFS.jsx
@@ -1,48 +1,49 @@
-import { useState, useEffect   } from "react";
+import { useState, useEffect } from "react";
 import { NFTStorage } from "nft.storage";
 
-const UploadIPFS = ({ setFileUrl }) => {
-   const [img, setImg] = useState([]);
-   const [isUploading, setIsUploading] = useState(false);
+const UploadIPFS = ({ setFileUrl, setUploaded }) => {
+  const [img, setImg] = useState([]);
+  const [isUploading, setIsUploading] = useState(false);
 
-   const upload = async () => {
-      try {
-         const client = new NFTStorage({
-            token: process.env.NEXT_PUBLIC_NFT_STORAGE,
-         });
-         const blob = new Blob([img], { type: img.type });
-         setIsUploading(true);
-         const imageCid = await client.storeBlob(blob);
-         const url = `https://ipfs.io/ipfs/${imageCid}`;
-         console.log("NFT metadata uploaded to IPFS : ", url);
-         setFileUrl(imageCid);
-      } catch (err) {
-         console.log(err);
-      } finally {
-         setIsUploading(false);
-      }
-   };
+  const upload = async () => {
+    try {
+      const client = new NFTStorage({
+        token: process.env.NEXT_PUBLIC_NFT_STORAGE,
+      });
+      const blob = new Blob([img], { type: img.type });
+      setIsUploading(true);
+      const imageCid = await client.storeBlob(blob);
+      const url = `https://ipfs.io/ipfs/${imageCid}`;
+      console.log("NFT metadata uploaded to IPFS : ", url);
+      setFileUrl(imageCid);
+      setUploaded(true);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsUploading(false);
+    }
+  };
 
-   useEffect(() => {
-      if (img.length !== 0){
+  useEffect(() => {
+    if (img.length !== 0) {
       upload();
-      }
-   }, [img]);
+    }
+  }, [img]);
 
-   return (
+  return (
+    <div>
       <div>
-         <div>
-            <label>
-               <input
-                  type="file"
-                  onChange={(e) => {
-                     setImg(e.target.files[0]);
-                  }}
-               ></input>
-            </label>
-         </div>
+        <label>
+          <input
+            type="file"
+            onChange={(e) => {
+              setImg(e.target.files[0]);
+            }}
+          ></input>
+        </label>
       </div>
-   );
+    </div>
+  );
 };
 
 export default UploadIPFS;
